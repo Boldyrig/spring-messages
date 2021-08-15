c4aab48a03c8313e581f5d22ae73e050f70547e0
Boldyrig <fuskerr63@gmail.com>
2021-08-15 21:59:37 +0400

    Initial commit

---
+++ src/test/java/com/gmail/fuskerr/springdev/SpringDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.fuskerr.springdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
